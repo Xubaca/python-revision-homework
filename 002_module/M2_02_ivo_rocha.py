460028a4b3eefaddc7ff61ca5009e20369335c09
Xubaca <ivo.xmr@protonmail.com>
2025-06-30 15:08:20 +0100

    exercise 1

---
+++ 002_module/M2_02_ivo_rocha.py
@@ -0,0 +1,13 @@
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    first :float = 5.0
+    second :float = 6.0
+    third :float = 7.0
+    #printing the average
+    print(f"a nota média é {(first + second + third)/3}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
