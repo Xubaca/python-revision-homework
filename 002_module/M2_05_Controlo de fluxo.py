7bfcccd4e8e10ed0b6a589cbbc6dfcf35e547a28
Xubaca <ivo.xmr@protonmail.com>
2025-07-18 19:41:16 +0100

    issue corrected on the while loop

--- 002_module/M2_05_Controlo de fluxo.py
+++ 002_module/M2_05_Controlo de fluxo.py
@@ -3,20 +3,19 @@
 #* Se há um erro devido a que o primeiro número introduzido é 0
 
 def pergunta_1():
-    numeros :list = ()
+    numeros :list = []
     #um while para ser possivel repetir respostas caso ocorra o erro do 0
-    while True:
+    iterator :int = 0
+    while iterator<3:
         resposta = int(input("Digite os numeros: "))
         #vaildação se o primeiro numero é de facto 0
         if(len(numeros) == 0 and resposta == 0):
             print("O primeiro numero não pode ser 0!")
-            continue
-        #appned
-        if(len(numeros) <= 3):
-            numeros.append(resposta)
-        #exit
+        #append
         else:
-            break
+            iterator += 1
+            numeros.append(resposta)
+        print(numeros)
 
     #+=1 se o numero é menor que o seguinte ou -=1
     order :int = 0
@@ -51,3 +50,4 @@ def pergunta_1():
 
 
 
+pergunta_1()
